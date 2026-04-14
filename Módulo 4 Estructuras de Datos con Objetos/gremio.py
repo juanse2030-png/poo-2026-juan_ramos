4a17b9a7ec78dc6cec4a2639f46292cf2c040f59
juanse2030 <juansebastianramosovalleramoso@gmail.com>
2026-04-13 21:56:52 -0500

    Update gremio.py

--- Módulo 4 Estructuras de Datos con Objetos/gremio.py
+++ Módulo 4 Estructuras de Datos con Objetos/gremio.py
@@ -1,53 +1,31 @@
-class Personaje:
-    def __init__(self, nombre, nivel, hp):
-        self.nombre = nombre
-        self.nivel = nivel
-        self.hp = hp
-        self.estado = "activo"
-        class GremioRPG:
-    def __init__(self, nombre):
-        self.nombre_gremio = nombre
-        self.equipo_principal = [None] * 6
-        self.dungeon = [[None]*3 for _ in range(3)]
-        self.espera = []
-
-    def unirse_al_equipo(self, p):
-        for i in range(len(self.equipo_principal)):
-            if self.equipo_principal[i] is None:
-                self.equipo_principal[i] = p
-                print(f'{p.nombre} se unió al equipo.')
-                return True
-        self.espera.append(p)
-        print(f'{p.nombre} en espera.')
-        return False
-
-    def colocar_en_dungeon(self, p, fila, col):
-        if self.dungeon[fila][col] is None:
-            self.dungeon[fila][col] = p
-
-    def limpiar_caidos(self):
-        for i in range(len(self.equipo_principal)):
-            p = self.equipo_principal[i]
-            if p is not None and p.hp == 0:
-                print(f'{p.nombre} ha caído.')
-                self.equipo_principal[i] = None
-
-        self.espera = [p for p in self.espera if p.hp > 0]
-
-    def reporte_gremio(self):
-        activos = sum(1 for p in self.equipo_principal if p is not None)
-        hp_total = sum(p.hp for p in self.equipo_principal if p is not None)
-
-        print(f'\n=== GREMIO: {self.nombre_gremio} ===')
-        print(f'Equipo activo: {activos}/6')
-        print(f'HP total equipo: {hp_total}')
-        print(f'En espera: {len(self.espera)}')
-        g = GremioRPG("Avengers")
+g = GremioRPG("Avengers")
 
 p1 = Personaje("Thor", 12, 150)
 p2 = Personaje("Hulk", 15, 200)
+p3 = Personaje("Iron Man", 11, 140)
+p4 = Personaje("Spiderman", 9, 100)
+p5 = Personaje("Capitan America", 13, 160)
+p6 = Personaje("Vision", 14, 170)
+p7 = Personaje("black panther", 8, 90)
+p8 = Personaje("Hawkeye", 7, 80)
 
 g.unirse_al_equipo(p1)
 g.unirse_al_equipo(p2)
+g.unirse_al_equipo(p3)
+g.unirse_al_equipo(p4)
+g.unirse_al_equipo(p5)
+g.unirse_al_equipo(p6)
+g.unirse_al_equipo(p7)
+g.unirse_al_equipo(p8)
 
+g.colocar_en_dungeon(p1, 0, 0)
+g.colocar_en_dungeon(p2, 0, 1)
+g.colocar_en_dungeon(p3, 1, 1)
+g.colocar_en_dungeon(p4, 2, 2)
+
+# Simulación de batalla
+p1.hp = 0
+p2.hp = 0
+
+g.limpiar_caidos()
 g.reporte_gremio()
